c872d409342b2856890442ab506c3d8348c3e997
Antonio <kiraanto@hotmail.it>
2022-04-20 22:13:57 +0200

    Initial Commit

---
+++ omniscient/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  omniscient
+//
+//  Created by Antonio Langella on 20/04/22.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
