eb4f3f04e7391de9e99adeee550819093867ae44
my-rice <dvdrsi2017@gmail.com>
2022-04-24 11:51:24 +0200

    aaa3

---
+++ omniscient/controllers/SettingsStoryboardController.swift
@@ -0,0 +1,21 @@
+//
+//  SettingsStoryboardController.swift
+//  omniscient
+//
+//  Created by DAVIDE RISI on 24/04/22.
+//
+
+import UIKit
+
+class SettingsStoryboardController: UIViewController {
+
+    @IBOutlet weak var myLabel: UILabel!
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+        
+    }
+
+
+}
