fd480202865721532675e51b91a309193ab232ad
Antonio <kiraanto@hotmail.it>
2022-05-27 11:42:55 +0200

    .

---
+++ omniscient/controllers/HomeController.swift
@@ -0,0 +1,17 @@
+//
+//  HomeController.swift
+//  omniscient
+//
+//  Created by Antonio Langella on 27/05/22.
+//
+
+import Foundation
+
+class HomeController: UIViewController {
+    
+}
+
+
+class RoomTableCell: UITableViewCell {
+    
+}
---
+++ omniscient/controllers/RoomController.swift
@@ -0,0 +1,16 @@
+//
+//  RoomController.swift
+//  omniscient
+//
+//  Created by Antonio Langella on 27/05/22.
+//
+
+import Foundation
+
+class RoomController: UIViewController {
+    
+}
+
+class SensorTableCell: UITableViewCell { //nota: provare prima con CollectionView
+    
+}
