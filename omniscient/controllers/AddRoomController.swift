fa6dbe488642d2c30be4f79cc05f5ef9ecc2d02f
My-Rice <dvdrsi2017@gmail.com>
2022-06-01 11:07:55 +0200

    Pusho l'addRoomController

---
+++ omniscient/controllers/AddRoomController.swift
@@ -0,0 +1,14 @@
+//
+//  AddRoomController.swift
+//  omniscient
+//
+//  Created by DAVIDE RISI on 31/05/22.
+//
+
+import UIKit
+
+class AddRoomController: UIViewController{
+    override func viewDidLoad() {
+        super.viewDidLoad()
+    }
+}
